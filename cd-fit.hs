534b39707330f61b8eff4f5299961e45e920d3c2
oklaridentitet <oklaridentitet@gmail.com>
2014-12-15 22:37:58 +0100

    actually parsing

--- cd-fit.hs
+++ cd-fit.hs
@@ -18,10 +18,13 @@ data Dir = Dir Int String deriving Show
 dirAndSize =
     do size <- many1 digit
        spaces
-       dir_names <- anyChar `manyTill` newline
+       dir_name <- anyChar `manyTill` newline
        return (Dir (read size) dir_name)
 
-main = do name <- getContents
-       	  putStrLn ("Hello. " ++ name)
-          color <- getContents
-       	  putStrLn ("Hello. " ++ color)
+main = do input <- getContents
+       	  putStrLn ("DEBUG: got input " ++ input)
+          let dirs = case parse parseInput "stdin" input of
+                       Left err -> error $ "Input:\n" ++ show input ++
+                                           "\nError:\n" ++ show err
+                       Right result -> result
+          putStrLn "DEBUG: parsed:"; print dirs
